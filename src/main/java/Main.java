868f4045d480188f34f5a0b233079e4a44426ffb
Omikorin <michal.k2001@gmail.com>
2022-06-01 02:07:43 +0200

    add TypDnia

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,16 @@ import java.util.Arrays;
 import java.util.Random;
 
 public class Main {
-	static ArrayList<Student> make_student_list(int ilosc) {
+	public final static int LICZBA_DNI_SEMESTRU = 100;
+	public final static int LICZBA_DNI_SESJI = 20;
+	public final static int LICZBA_KOLOKWIOW = 4;
+	public final static int DZIEN_SESJI = LICZBA_DNI_SEMESTRU - LICZBA_DNI_SESJI + 1;
+
+	public static int semestr = 1;
+	public static int dzien = 1;
+	public static int kolokwium = 1;
+
+	public static ArrayList<Student> make_student_list(int ilosc) {
 		ArrayList<Student> list = new ArrayList<>();
 		Random generator = new Random();
 
@@ -16,15 +25,31 @@ public class Main {
 		return list;
 	}
 
+	public static TypDnia sprawdzDzien() {
+		if (dzien >= DZIEN_SESJI) {
+			return TypDnia.SESJA;
+		}
+
+		int dzienPierwszegoKolowkium = (LICZBA_DNI_SEMESTRU - LICZBA_DNI_SESJI) / LICZBA_KOLOKWIOW;
+
+		for (int i = dzienPierwszegoKolowkium; i < DZIEN_SESJI; i += dzienPierwszegoKolowkium) {
+			if (dzien == i) {
+				return TypDnia.KOLOKWIUM;
+			}
+		}
+		
+		return TypDnia.ZWYKLY;
+	}
+
 	public static void main(String[] args) {
 		Plansza plansza = new Plansza(100, 100);
-	
+
 		ArrayList<Student> list = make_student_list(10);
-		
+
 		Plansza.placeObjectsInRandomOrder(list);
-		
+
 		Piwo kustosz = new Piwo(2, 2, 2);
-		
+
 		for (Student student : list) {
 			System.out.println(Arrays.toString(student.getCoordinates()));
 		}
---
+++ src/main/java/TypDnia.java
@@ -0,0 +1,6 @@
+
+public enum TypDnia {
+	ZWYKLY,
+	KOLOKWIUM,
+	SESJA;
+}
